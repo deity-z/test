dc5cba0923ad31048f8d00f8cd620334a946bd5f
伍智 <7621551+ELALS@user.noreply.gitee.com>
2021-04-19 20:22:18 +0800

    'xxx'

---
+++ src/main/java/com/bdqn/nursing/NewNursingHomeApplication.java
@@ -0,0 +1,13 @@
+package com.bdqn.nursing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewNursingHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewNursingHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bdqn/nursing/NewNursingHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdqn.nursing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewNursingHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
